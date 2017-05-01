3afae782f5cf4339cb1d2f8e00bcc22fe92a40bd
addykumar27 <adarshna27@yahoo.com>
2017-04-30 20:47:25 -0700

    temporary changes

---
+++ public/scripts/app.js
@@ -0,0 +1,28 @@
+console.log('sanity check');
+var myApp = angular.module('bookApp', ['ngRoute'])
+.config(config);
+console.log('show book');
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config (  $routeProvider,   $locationProvider  )  {
+  $routeProvider
+    .when('/', {
+      templateUrl: '../../templates/booksindex.html',
+      controller: 'booksIndexController',
+      controllerAs: 'booksIndexCtrl'  
+    })
+    .when('/pokemons/:id', {
+      templateUrl: '../../templates/booksshow.html',
+      controller: 'booksShowController',
+      controllerAs: 'booksShowCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  $locationProvider
+    .html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+}
